e7ff90a7a16c00ead5b08eb7c3c1df600a38b383
Sebastian Wallace <sebjwallace@gmaill.com>
2020-12-08 16:57:42 +0000

    two services working with kafka inside minikube

---
+++ readme.md
@@ -0,0 +1,1 @@
+https://technology.amis.nl/2019/03/24/running-apache-kafka-on-minikube/
---
+++ services/carrier/src/index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const { Kafka } = require('kafkajs')
+
+const topic = 'first'
+
+let connection = null
+let producer = null
+let consumer = null
+
+app.get('/send', async (req, res) => {
+
+    await producer.send({
+        topic,
+        messages: [
+            { value: req.query.message }
+        ]
+    })
+
+    res.send('sent ' + req.query.message)
+})
+
+app.listen(port, async () => {
+	console.log('listening on 3000')
+
+	connection = new Kafka({
+		clientId: 'app',
+		brokers: ['kafka.kafka-ca1:9092']
+	})
+
+	producer = connection.producer({ acks: -1 }) // all replicas ack
+	await producer.connect()
+
+	consumer = connection.consumer({ groupId: 'test-group' })
+	await consumer.connect()
+	await consumer.subscribe({ topic })
+	await consumer.run({
+		eachMessage: async ({ topic, partition, message }) => {
+			console.log({
+				consumer: 'carrier',
+				value: message.value.toString()
+			})
+		}
+	})
+	
+	console.log('carrier: kafka ready')
+})
